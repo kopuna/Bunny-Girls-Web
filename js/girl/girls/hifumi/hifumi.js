ff182b8d4a3fd56dd421b9919044b051dc03c588
kopuna <wotwillmail@gmail.com>
2025-05-23 00:04:53 +0200

    feat(all): add 2 dummy profiles

---
+++ js/girl/girls/hifumi/hifumi.js
@@ -0,0 +1,50 @@
+const hifumi = new girl(
+    {
+        name: 'Hifumi',
+        services: '',
+        shoe: '',
+        barefoot: '',
+        inshoe: '',
+        butt: '',
+        breast: '',
+        hands: '',
+        swallow: '',
+        chewing: '',
+        fWorship: '',
+        fMassage: '',
+        anitacids: '',
+        toenails: '',
+        description: '',
+        quote: '',
+        aboutHer: '',
+        vores: '',
+        crushes: '',
+        height: '',
+        weight: '',
+        footSize: '',
+        questions: [
+            'Q - What\'s your favorite type of service?',
+            'Q - What is your ideal type of customer?',
+            'Q - What was your best experience with a customer?',
+            'Q - What do you think about your clients? Do you feel bad for killing them?',
+            'Q - Do you like being a Bunny Girl? What do you like the most of this job? And why did you choose to work here?'
+        ],
+        answers: [
+            '',
+            '',
+            '',
+            '',
+            ''
+        ],
+          
+    },
+    '../../../assets/Logo.png',
+    './../../../whoWeAre.html',
+    [
+        { text: 'Location', href: '#' },
+        { text: 'About us', href: './../../../whoWeAre.html' },
+        { text: 'Our girls', href: './../ourGirls.html' },
+        { text: 'Gallery', href: '#' },
+        { text: 'Work with us', href: '#' }
+    ]
+);
