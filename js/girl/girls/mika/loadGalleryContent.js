40b61f2031aba899c390be5a00b4b8068c374cd7
kopuna <wotwillmail@gmail.com>
2025-05-24 21:50:20 +0200

    feat(views): add gallery for mika

--- js/girl/girls/mika/loadGalleryContent.js
+++ js/girl/girls/mika/loadGalleryContent.js
@@ -1,6 +1,25 @@
 const assetData = [
     [
-        '',
+        './../../../../assets/gallery/iori/relaxWithMika/Page0.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page1.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page2.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page3.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page4.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page5.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page6.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page7.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page8.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page9.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page10.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page11.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page12.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page13.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page14.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page15.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page16.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page17.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page18.jpg',
+        './../../../../assets/gallery/iori/relaxWithMika/Page19.jpg',
     ],
 ]
 
